6c41c397a187a5cc6ab17621d80226fc40df08e6
Ahmed AbdElrazek Ahmed <admin@yallagroup.net>
2018-05-01 19:29:23 +0200

    handle project routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,37 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', 'Frontend\PagesController@homepage')->name('home');
+
+Route::get('/info/{page}', 'Frontend\PagesController@show')->name('page.show');
+Route::post('/info/contact', 'Frontend\PagesController@contact')->name('page.contact');
+Route::get('/category/{id}/{slug}', 'Frontend\CategoriesController@show')->name('category.show');
+Route::get('/program/{id}/{slug}', 'Frontend\ProgramsController@show')->name('program.show');
+
+
+//=> admin routes
+Route::prefix('admin')->middleware(['auth'])->namespace('Backend')->group(function () {
+
+    //=> categories routes
+    Route::prefix('category')->group(function () {
+        Route::get('index', 'CategoriesController@index')->name('categories.index');
+        Route::get('create', 'CategoriesController@create')->name('category.create');
+        Route::post('store', 'CategoriesController@store')->name('category.store');
+        Route::get('{id}/edit', 'CategoriesController@edit')->name('category.edit');
+        Route::post('{id}/update', 'CategoriesController@update')->name('category.update');
+        Route::get('{id}/delete', 'CategoriesController@destroy')->name('category.destroy');
+    });
+
+    //=> programs routes
+    Route::prefix('program')->group(function () {
+        Route::get('index', 'ProgramsController@index')->name('programs.index');
+        Route::get('create', 'ProgramsController@create')->name('program.create');
+        Route::post('store', 'ProgramsController@store')->name('program.store');
+        Route::get('{id}/edit', 'ProgramsController@edit')->name('program.edit');
+        Route::post('{id}/update', 'ProgramsController@update')->name('program.update');
+        Route::get('{id}/delete', 'ProgramsController@destroy')->name('program.destroy');
+    });
+
 });
