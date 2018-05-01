407a32c61ca12d1dcd90e89a6389ae102c6df9db
Ahmed AbdElrazek Ahmed <admin@yallagroup.net>
2018-05-01 20:26:58 +0200

    handle models and make parent master model

--- app/Master.php
+++ app/Master.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Page extends Master
+class Master extends Model
 {
-
+    protected $guarded = ['id'];
 }
--- app/Program.php
+++ app/Program.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Program extends Model
+class Program extends Master
 {
-    //
+
 }
