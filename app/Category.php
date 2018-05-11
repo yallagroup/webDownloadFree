b3d2bef4d46b73800be5f29f75e2c012465ed1e2
Ahmed AbdElrazek Ahmed <krinkel@gmail.com>
2018-05-11 20:15:35 +0200

    handle model relations

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Master
 {
 
+	public function childs()
+	{
+		return $this->hasMany(Category::class, 'parent_id')->get();
+	}
+
+	public function programs()
+	{
+		return $this->hasMany(Program::class)->get();
+	}
+
 }
--- app/Program.php
+++ app/Program.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Program extends Master
 {
 
+	public function category()
+	{
+		return $this->belongsTo(Category::class);
+	}
+
+	public function user()
+	{
+		return $this->belongsTo(User::class);
+	}
+
 }
